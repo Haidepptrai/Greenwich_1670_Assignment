84e3ba624901d59be2887711f958cbc51e07a5a3
Hai Nguyen <101555643+Haidepptrai@users.noreply.github.com>
2024-04-13 11:20:11 +0700

    Revert "style: Home Page Layout"

--- FPT_Pharmacy_Assignment/Controllers/UsersController.cs
+++ FPT_Pharmacy_Assignment/Controllers/UsersController.cs
@@ -1,7 +1,12 @@
-﻿using FPT_Pharmacy_Assignment.Data;
-using FPT_Pharmacy_Assignment.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using FPT_Pharmacy_Assignment.Data;
+using FPT_Pharmacy_Assignment.Models;
 
 namespace FPT_Pharmacy_Assignment.Controllers
 {
