4df33f753a0dd5f8f93df60cc44cc9c821ed30a1
Haidepptrai <hainguyenhoangcontact@gmail.com>
2024-04-22 13:44:29 +0700

    Seed data for manufacturer and product, also add layout for the products view

--- FPT_Pharmacy_Assignment/Areas/Admin/Models/SeedData.cs
+++ FPT_Pharmacy_Assignment/Areas/Admin/Models/SeedData.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using FPT_Pharmacy_Assignment.Data;
+using Microsoft.AspNetCore.Identity;
 
 namespace FPT_Pharmacy_Assignment.Areas.Admin.Models
 {
@@ -7,6 +8,7 @@ namespace FPT_Pharmacy_Assignment.Areas.Admin.Models
         public static async Task Initialize(IServiceProvider serviceProvider)
         {
             var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var context = serviceProvider.GetRequiredService<FPT_Pharmacy_AssignmentContext>();
 
             string[] roleNames = ["Admin", "Moderator", "User"];
             foreach (var roleName in roleNames)
@@ -16,6 +18,68 @@ namespace FPT_Pharmacy_Assignment.Areas.Admin.Models
                 {
                     await roleManager.CreateAsync(new IdentityRole(roleName));
                 }
+            }
+
+            if (!context.Manufacturer.Any())
+            {
+                context.Manufacturer.AddRange(
+                    new Manufacturer
+                    {
+                        Name = "Manufacturer 1",
+                        Phone = "1234567890"
+                    },
+
+                    new Manufacturer
+                    {
+                        Name = "Manufacturer 2",
+                        Phone = "9876543210"
+                    },
+
+                    new Manufacturer
+                    {
+                        Name = "Manufacturer 3",
+                        Phone = "5555555555"
+                    }
+                );
+                await context.SaveChangesAsync();
+            }
+
+
+            // Look for any products.
+            if (!context.Product.Any())
+            {
+                context.Product.AddRange(
+                    new Product
+                    {
+                        ManufacturerId = 1,
+                        ImageFile = "image1.jpg",
+                        Name = "Product 1",
+                        Description = "Description for Product 1",
+                        Price = 10.99m,
+                        StockLevel = 100
+                    },
+
+                    new Product
+                    {
+                        ManufacturerId = 1,
+                        ImageFile = "image2.jpg",
+                        Name = "Product 2",
+                        Description = "Description for Product 2",
+                        Price = 20.99m,
+                        StockLevel = 200
+                    },
+
+                    new Product
+                    {
+                        ManufacturerId = 1,
+                        ImageFile = "image3.jpg",
+                        Name = "Product 3",
+                        Description = "Description for Product 3",
+                        Price = 30.99m,
+                        StockLevel = 300
+                    }
+                );
+                await context.SaveChangesAsync();
             }
         }
     }
---
+++ FPT_Pharmacy_Assignment/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using FPT_Pharmacy_Assignment.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FPT_Pharmacy_Assignment.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly FPT_Pharmacy_AssignmentContext _context;
+
+        public ProductsController(FPT_Pharmacy_AssignmentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Products
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Product.ToListAsync());
+        }
+
+        // GET: Products/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Product
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
